4bc9f463f59c99a68840aee669e4ce34fd3913a4
BayLadyCoder <rachada_32@hotmail.com>
2021-03-22 15:33:45 -0400

    Styled Item component

--- src/Item/Item.styles.ts
+++ src/Item/Item.styles.ts
@@ -1,3 +1,36 @@
 import styled from 'styled-components'
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
+    width:100%;
+    border: 1px solid #cccccc;
+    border-radius: 20px;
+    height: 100%;
+    color: #383838;
+
+    button {
+        border-radius: 0 0 20 20px;
+        font-weight: bold;
+        color: #0a56c7;
+        background-color: #f2f8ff;
+    }
+    button:hover {
+        color: #005bc4;
+        background-color: #dbeaff;
+    }
+
+    img {
+        margin-top: 1.5rem;
+        max-height: 200px;
+        object-fit: contain;
+        border-radius: 20px 20px 0 0;
+    }
+    
+    div {
+        font-family: Arial, Helvetica, sans-serif;
+        padding: 1.5rem;
+        height: 100%
+    }
+`;
