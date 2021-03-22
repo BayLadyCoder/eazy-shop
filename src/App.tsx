db3610984b6cde69329b58d7c908e64348bb5429
BayLadyCoder <rachada_32@hotmail.com>
2021-03-22 15:03:13 -0400

    Use QueryClient and fetch data from FakeStoreAPI

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,25 @@
-import "./App.css";
+import { useQuery } from "react-query";
+
+//Types
+export type CartItemType = {
+  id: number;
+  title: string;
+  description: string;
+  category: string;
+  price: number;
+  image: string;
+  amount: number;
+};
+
+const getProducts = async (): Promise<CartItemType[]> =>
+  await (await fetch("https://fakestoreapi.herokuapp.com/products")).json();
 
 const App = () => {
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    "product",
+    getProducts
+  );
+  console.log(data);
   return <div className="App">Start</div>;
 };
 
