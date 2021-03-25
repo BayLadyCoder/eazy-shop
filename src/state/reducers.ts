21fabefc0f153cc8875e4e76ac012fe3ec20a233
BayLadyCoder <rachada_32@hotmail.com>
2021-03-25 00:17:14 -0400

    Fixed cart item amount 0 in cart bug

--- src/state/reducers.ts
+++ src/state/reducers.ts
@@ -1,84 +1,82 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from "redux";
 import {
-    API_GET_ALL_PRODUCTS_SUCCESS,
-    // API_GET_ALL_PRODUCTS_FAIL,
-    ADD_ITEM_TO_CART,
-    REMOVE_ITEM_FROM_CART,
-} from './actions'
+  API_GET_ALL_PRODUCTS_SUCCESS,
+  // API_GET_ALL_PRODUCTS_FAIL,
+  ADD_ITEM_TO_CART,
+  REMOVE_ITEM_FROM_CART,
+} from "./actions";
 
 // Types
-import { CartItemType, ProductState, CartState, Actions } from '../Types'
-
+import { CartItemType, ProductState, CartState, Actions } from "../Types";
 
 const ProductInitialState = {
-    products: [],
-    isLoading: true
-}
+  products: [],
+  isLoading: true,
+};
 const CartInitialState = {
-    items: [],
-}
+  items: [],
+};
 
+export const productReducer = (
+  state: ProductState = ProductInitialState,
+  action: Actions
+): ProductState => {
+  const { type, payload } = action;
 
+  switch (type) {
+    case API_GET_ALL_PRODUCTS_SUCCESS:
+      return { products: payload, isLoading: false };
+    default:
+      return state;
+  }
+};
 
-export const productReducer = (state: ProductState = ProductInitialState, action: Actions): ProductState => {
-    const { type, payload } = action;
+export const cartReducer = (
+  state: CartState = CartInitialState,
+  action: Actions
+): CartState => {
+  const { type, payload } = action;
+  let newStateItems: CartItemType[];
 
-    switch (type) {
-        case API_GET_ALL_PRODUCTS_SUCCESS:
-            return { products: payload, isLoading: false }
-        default:
-            return state
-
-    }
-}
-
-
-
-export const cartReducer = (state: CartState = CartInitialState, action: Actions): CartState => {
-    const { type, payload } = action;
-    let newStateItems: CartItemType[];
-
-    switch (type) {
-        case ADD_ITEM_TO_CART:
-            const isItemInCart = state.items.find((item) => item.id === payload?.id);
-            if (isItemInCart) {
-                newStateItems = state.items.map((item) => {
-                    if (item.id === payload?.id) {
-                        return { ...item, amount: item.amount + 1 };
-                    } else {
-                        return item;
-                    }
-                });
+  switch (type) {
+    case ADD_ITEM_TO_CART:
+      const isItemInCart = state.items.find((item) => item.id === payload?.id);
+      if (isItemInCart) {
+        newStateItems = state.items.map((item) => {
+          if (item.id === payload?.id) {
+            return { ...item, amount: item.amount + 1 };
+          } else {
+            return item;
+          }
+        });
+      } else {
+        newStateItems = [...state.items, { ...payload, amount: 1 }];
+      }
+      return { ...state, items: newStateItems };
+    case REMOVE_ITEM_FROM_CART:
+      newStateItems = state.items.reduce(
+        (acc: CartItemType[], item: CartItemType) => {
+          if (item.id === payload) {
+            if (item.amount === 1) {
+              return acc.filter((item) => item.id !== payload.id);
             } else {
-                newStateItems = [...state.items, { ...payload, amount: 1 }];
+              return [...acc, { ...item, amount: item.amount - 1 }];
             }
-            return { ...state, items: newStateItems }
-        case REMOVE_ITEM_FROM_CART:
-            newStateItems = state.items.reduce((acc: CartItemType[], item: CartItemType) => {
-                if (item.id === payload) {
-                    if (item.amount === 0) {
-                        return acc;
-                    } else {
-                        return [...acc, { ...item, amount: item.amount - 1 }];
-                    }
-                } else {
-                    return [...acc, item];
-                }
-            }, [] as CartItemType[])
-            return { ...state, items: newStateItems }
-        default:
-            return state
-
-    }
-}
-
-
-
-
+          } else {
+            return [...acc, item];
+          }
+        },
+        [] as CartItemType[]
+      );
+      return { ...state, items: newStateItems };
+    default:
+      return state;
+  }
+};
 
 const rootReducer = combineReducers({
-    products: productReducer,
-    cart: cartReducer
-})
+  products: productReducer,
+  cart: cartReducer,
+});
 
 export default rootReducer;
